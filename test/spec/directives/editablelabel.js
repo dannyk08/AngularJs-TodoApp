fc7e16f2083be0ad8c6afbeadbdd11876be1bea1
Rohit Ghatol <rohit.ghatol@synerzip.com>
2015-01-07 18:49:32 -0800

    Attempting to make directive test case work when using templateUrl but it is not working yet

--- test/spec/directives/editablelabel.js
+++ test/spec/directives/editablelabel.js
@@ -2,19 +2,24 @@
 
 describe('Directive: EditableLabel', function () {
 
-  // load the directive's module
-  beforeEach(module('todoApp'));
+    // load the directive's module
+    beforeEach(module('todoApp'));
+    beforeEach(module('app/views/editablelable.html','app/views/editablelable.html'));
 
-  var element,
-    scope;
+    var element,
+        scope;
 
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
-  }));
+    beforeEach(inject(function ($rootScope) {
+        scope = $rootScope.$new();
+    }));
 
-  it('should make hidden element visible', inject(function ($compile) {
-    element = angular.element('<-editable-label></-editable-label>');
-    element = $compile(element)(scope);
-    expect(element.text()).toBe('this is the EditableLabel directive');
-  }));
+
+    it('should show Text', inject(function ($compile) {
+        scope.value='ABC';
+        element = angular.element('<editable-label value="Test"></editable-label>');
+        element = $compile(element)(scope);
+        scope.$digest();
+        console.log(element.html());
+        expect(element.text()).toBe('ABC');
+    }));
 });
